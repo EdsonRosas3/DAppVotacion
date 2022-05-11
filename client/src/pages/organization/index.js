ac73f1648e54beca2554f7539c6319d9a4cf8742
XDMysterion1 <XDMysterion@gmail.com>
2022-05-10 23:50:48 -0400

    Agregar organizacion

--- client/src/pages/organization/index.js
+++ client/src/pages/organization/index.js
@@ -14,6 +14,9 @@ import {
   AutoComplete,
 } from 'antd';
 
+import { Menu, Dropdown, message, Space, Tooltip } from 'antd';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+
 const Organization = () => {
 
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -36,6 +39,35 @@ const Organization = () => {
   const onFinish = (values) => {
     console.log('Received values of form: ', values);
   };
+
+  function handleMenuClick(e) {
+    message.info('Click on menu item.');
+    console.log('click', e);
+  }
+
+  const menu = (
+    <Menu
+      onClick={handleMenuClick}
+      items={[
+        {
+          label: 'Nacional',
+          key: '1',
+        },
+        {
+          label: 'Departamental',
+          key: '2',
+        },
+        {
+          label: 'Municipal',
+          key: '3',
+        },
+        {
+          label: 'Vecinal',
+          key: '4',
+        },
+      ]}
+    />
+  );
   return (
     <Card style={{ width: "100%" }}  bordered={true}>
       <Button type="primary" onClick={showModal}>
@@ -60,6 +92,23 @@ const Organization = () => {
         >
           <Input />
         </Form.Item>
+
+        <Form.Item
+          name="description"
+          label="descripción"
+          tooltip="¿Que deescripcion tiene?"
+          rules={[{ required: true, message: '¡Por favor ingrese la descripcion!', whitespace: true }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Dropdown overlay={menu} >
+          <Button>
+            <Space>
+              Alcance
+            </Space>
+          </Button>
+        </Dropdown>
 
       </Form>
       </Modal>
